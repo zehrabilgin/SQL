d9cdab3ae0f9cc8305843b8cd043cd46d1e7fd17
zehra.bilgin <zehrabilgin341@gmail.com>
2022-03-14 11:07:53 +0300

    Sixth Homework

---
+++ odev6.sql
@@ -0,0 +1,13 @@
+---1
+SELECT avg(rental_rate) from film;
+
+---2
+select count(*) from film where title like 'C%';
+
+---3
+select max(length) from film
+where rental_rate=0.99;
+
+---4
+select count(distinct(replacement_cost)) from film
+where length>150;
